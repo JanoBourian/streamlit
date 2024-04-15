057bedbde781223a22a229c46df8029631bead9d
JanoBourian <supermath33@icloud.com>
2024-04-15 15:52:01 -0600

    add: fundamental

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # streamlit
+
+## Starting
+
+```bash
+python -m streamlit run your_script.py
+streamlit run your_script.py
+```
+
+##  Useful links 
+
+* [API reference](https://docs.streamlit.io/develop/api-reference)
+* [Create Docker File](https://docs.streamlit.io/deploy/tutorials/docker#create-a-dockerfile)
+* [Cheat sheet](https://cheat-sheet.streamlit.app/)
---
+++ app.py
@@ -0,0 +1,165 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+import time
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+# Use magic
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+# st.table(df)
+df
+
+# 
+st.write("Hello @janobourian from streamlit")
+
+# Write and dataframe
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+st.table(dataframe)
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+#
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
+
+# Draw a line chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+# Plot a map
+@st.cache_data
+def map_information():
+    map_data = pd.DataFrame(
+    # np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    np.random.randn(1000, 2) / [5, 5] + [19.432608, -99.133209],
+    columns=['lat', 'lon'])
+    
+    return map_data
+
+# st.map(map_data)
+st.map(map_information())
+
+# Widgets
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+# You can access the value at any point with:
+st.session_state.name
+
+print(st.session_state)
+print(dir(st.session_state))
+
+# Use checkboxes to show/hide data
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+# Use a selectbox for options
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+value = st.selectbox(
+    'Which number do you prefer?',
+     df['second column'])
+
+'You selected: ', option, 'Value: ', value
+
+# Layout
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+# Show progress
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
+
+# Stream data, Iterator and Generator
+_LOREM_IPSUM = """
+Lorem ipsum dolor sit amet, **consectetur adipiscing** elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+"""
+
+
+def stream_data():
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+    yield pd.DataFrame(
+        np.random.randn(5, 10),
+        columns=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+    )
+
+    for word in _LOREM_IPSUM.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+
+if st.button("Stream data"):
+    st.write_stream(stream_data)
